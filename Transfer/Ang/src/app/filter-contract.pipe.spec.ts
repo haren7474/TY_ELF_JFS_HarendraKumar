57276c0b4e4a21027c9df1f3b46b1a26cf324d05
haren7474 <harendra10104698@gmail.com>
2020-01-21 16:42:30 +0530

    Updated spring and angular will modal contract

--- Transfer/Ang/src/app/filter-contract.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { FilterContractPipe } from './filter-contract.pipe';
-
-describe('FilterContractPipe', () => {
-  it('create an instance', () => {
-    const pipe = new FilterContractPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
